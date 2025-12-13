be26e0f038f4c095358839391386f83c2df0ccd3
gregorku <gregorku@atlas.cz>
2025-12-13 21:44:15 +0100

    upgrade 13.12.2025

--- nixos-config/modules/common-snapshots.nix
+++ nixos-config/modules/common-snapshots.nix
@@ -1,12 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    copyKernels = true;
-    configurationLimit = 10;
-  };
-
   system.autoUpgrade.enable = false;
 }
