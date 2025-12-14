ab1f4951cf7e8ee0e8ef246306a05f522a669583
gregorku <gregorku@atlas.cz>
2025-12-14 20:00:03 +0100

    upgrade 14.12.2025-2

--- nixos-config/modules/common-desktop-kde.nix
+++ nixos-config/modules/common-desktop-kde.nix
@@ -1,28 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  # 🔑 ZÁKLAD GRAFIKY (SDDM + fallback X11)
   services.xserver.enable = true;
 
-  # 🔑 DISPLAY MANAGER + KDE Plasma 6
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # 🔊 AUDIO – PipeWire (25.05 správně)
+  # 🔑 XDG PORTAL – POVINNÉ PRO WAYLAND + PLASMA 6
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.kdePackages.xdg-desktop-portal-kde
+    ];
+  };
+
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
   };
 
-  # 🖥️ XDG Portals (Flatpak, screencast, dialogs)
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
-
-  # 🌐 NETWORK
   networking.networkmanager.enable = true;
 }
