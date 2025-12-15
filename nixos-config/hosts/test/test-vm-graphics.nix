e5f9462b89e7c89283ca1f2e0e85176daa9a5353
gregorku <gregorku@atlas.cz>
2025-12-15 20:42:09 +0100

    upgrade 15.12.2025-9

--- nixos-config/hosts/test/test-vm-graphics.nix
+++ nixos-config/hosts/test/test-vm-graphics.nix
@@ -1,25 +1,14 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  ##################################################
-  # VirtIO GPU + Wayland (VM)
-  ##################################################
+  hardware.opengl.enable = true;
 
   services.xserver.videoDrivers = [ "virtio" ];
-
-  # Wayland je default pro Plasma 6,
-  # ale explicitně ho povolíme
-  services.xserver.enable = true;
-
-  # Mesa + virgl (OpenGL akcelerace)
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
 
   environment.systemPackages = with pkgs; [
     mesa
     mesa-demos
+    vulkan-tools
+    virtiofsd
   ];
 }
