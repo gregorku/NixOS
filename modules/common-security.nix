521106d5b67ffac241509e44e42d2c74c46accc6
gregorku <gregorku@atlas.cz>
2025-12-19 06:48:19 +0100

    upgrade 19.12.2025-12

--- modules/common-security.nix
+++ modules/common-security.nix
@@ -1,12 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  networking.firewall = {
-    enable = false;
-    allowedTCPPorts = [ 22 ];
-    allowedUDPPorts = [];
-  };
+  ##################################################
+  # Firewall (ve VM pro testování vypnutý)
+  ##################################################
+  networking.firewall.enable = false;
 
+  ##################################################
+  # SSH
+  ##################################################
   services.openssh = {
     enable = true;
     openFirewall = false;
@@ -17,9 +19,30 @@
     };
   };
 
-  # Needed for PipeWire (realtime scheduling)
+  ##################################################
+  # ZÁKLAD pro Cockpit (POVINNÉ)
+  ##################################################
+
+  # System D-Bus
+  services.dbus.enable = true;
+
+  # PolicyKit – nutné pro session
+  security.polkit.enable = true;
+
+  # AccountsService – Cockpit bez něj padá po loginu
+  services.accounts-daemon.enable = true;
+
+  # PAM service pro Cockpit
+  security.pam.services.cockpit.enable = true;
+
+  ##################################################
+  # Realtime (nevadí)
+  ##################################################
   security.rtkit.enable = true;
 
+  ##################################################
   # Hardening
+  ##################################################
   security.protectKernelImage = true;
 }
+
