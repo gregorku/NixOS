b431d5d7d3b2c1dd89fd7ca0cf2db297e715de1b
gregorku <gregorku@atlas.cz>
2025-12-19 05:54:41 +0100

    upgrade 19.12.2025-7

--- modules/server/cockpit.nix
+++ modules/server/cockpit.nix
@@ -2,16 +2,28 @@
 
 {
   services.cockpit = {
-      enable = true;
-      port = 9090;
-      allowed-origins = [
-        "https://cockpit.<domain>.com"  # The public-facing URL clients will connect from in the browser
-      ];
-      settings = {
-        WebService = {
-          AllowUnencrypted = true;
-          ProtocolHeader = "X-Forwarded-Proto";  # Specifies the request goes through a reverse proxy
-        };
+    enable = true;
+    port = 9090;
+
+    # Odstranit allowed-origins pro přímé připojení (pokud nepoužíváte reverse proxy)
+    # allowed-origins = [ "https://cockpit.<domain>.com" ];  # ZAKOMENTOVÁNO
+
+    settings = {
+      WebService = {
+        AllowUnencrypted = true;  # Povolit HTTP pro testování
+
+        # Naslouchat na všech adresách (IPv4 i IPv6)
+        # Pokud chcete povolit pouze HTTP (bez HTTPS), přidejte:
+        # ProtocolHeader = "";  # Prázdné pro přímé připojení
+      };
     };
+  };
+
+  # Vynutit naslouchání na všech síťových rozhraních (IPv4 i IPv6)
+  systemd.sockets.cockpit.socketConfig = {
+    ListenStream = [
+      "0.0.0.0:9090"   # IPv4 na všech adresách
+      "[::]:9090"      # IPv6 na všech adresách
+    ];
   };
 }
