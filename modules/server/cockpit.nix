ab18aca3563812ae1ee024801706327335791ae5
gregorku <gregorku@atlas.cz>
2025-12-19 06:31:28 +0100

    upgrade 19.12.2025-10

--- modules/server/cockpit.nix
+++ modules/server/cockpit.nix
@@ -1,37 +1,15 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
   services.cockpit = {
     enable = true;
-    port = 9090;
-    openFirewall = false;  # Firewall je vypnutý, ale pro úplnost
+    openFirewall = true;
 
-    # Odstranit všechny omezení pro přístup
     settings = {
       WebService = {
-        AllowUnencrypted = true;
-        # Použijte mkForce k přepsání výchozí hodnoty
-        Origins = lib.mkForce "*";
+        # Poslouchej i na IPv4
+        ListenStream = "0.0.0.0:9090";
       };
     };
-  };
-
-  # KLÍČOVÉ: Explicitně definovat socket v NixOS stylu
-  systemd.sockets.cockpit = {
-    enable = true;
-    wantedBy = [ "sockets.target" ];
-    socketConfig = {
-      # TOTO JE NEJDŮLEŽITĚJŠÍ - přinutí IPv4 naslouchání
-      ListenStream = [ "0.0.0.0:9090" ];
-      # Pro IPv6 přidejte: "[::]:9090"
-      SocketMode = "0666";
-      PassCredentials = true;
-    };
-  };
-
-  # Zajistit, že socket bude spuštěn před službou
-  systemd.services.cockpit = {
-    requires = [ "cockpit.socket" ];
-    after = [ "cockpit.socket" ];
   };
 }
