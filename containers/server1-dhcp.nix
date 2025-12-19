c790741bc4d4bc22a65ec187513ee28e2d19b5fa
gregorku <gregorku@atlas.cz>
2025-12-19 20:49:44 +0100

    upgrade 19.12.2025-27

--- containers/server1-dhcp.nix
+++ containers/server1-dhcp.nix
@@ -3,53 +3,92 @@
 {
   containers.server1 = {
     autoStart = true;
-    privateNetwork = false;  # Sdílet hostitelskou síť
+    privateNetwork = false;  # Použít host network
+
+    # Přidat tyto flagy pro stabilitu
+    extraFlags = [
+      "--bind=/tmp"
+      "--bind=/run"
+      "--bind=/sys/fs/cgroup"
+    ];
 
     config = { config, pkgs, lib, ... }: {
       networking.hostName = "server1";
       system.stateVersion = "25.05";
 
-      # Povolit DHCP v kontejneru
-      networking.useDHCP = true;
+      ##################################################
+      # ŘEŠENÍ KONFLIKTU: Vynutit DHCP a vypnout NetworkManager
+      ##################################################
+      networking.networkmanager.enable = lib.mkForce false;  # VYPNOUT
+      networking.useDHCP = lib.mkForce true;  # VYNUTIT DHCP
 
+      # Explicitně zakázat všechny ruční síťové konfigurace
+      networking.dhcpcd.enable = false;
+      networking.interfaces = {};
+      networking.defaultGateway = lib.mkForce null;
+      networking.nameservers = lib.mkForce [];
+
+      ##################################################
       # Docker
+      ##################################################
       virtualisation.docker = {
         enable = true;
         enableOnBoot = true;
+        package = pkgs.docker_27;
+        daemon.settings = {
+          ip = "0.0.0.0";
+        };
       };
 
+      ##################################################
       # Portainer Business Edition
+      ##################################################
       systemd.services.portainer = {
-        description = "Portainer Business Edition";
+        description = "Portainer Business Edition 2.33.6";
         after = [ "docker.service" ];
         requires = [ "docker.service" ];
         serviceConfig = {
-          ExecStart = ''
-            ${pkgs.docker}/bin/docker run \
-              --name portainer \
-              --restart unless-stopped \
-              -p 8443:9000 \
-              -v /var/run/docker.sock:/var/run/docker.sock \
-              -v portainer_data:/data \
-              portainer/portainer-ee:2.33.6
-          '';
-          ExecStop = "${pkgs.docker}/bin/docker stop portainer";
-          ExecStopPost = "${pkgs.docker}/bin/docker rm portainer";
-          Restart = "on-failure";
+          Type = "simple";
+          ExecStart = "${pkgs.docker}/bin/docker run \
+            --name portainer \
+            --restart=always \
+            -p 8443:9000 \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v portainer_data:/data \
+            portainer/portainer-ee:2.33.6";
+          Restart = "always";
+          RestartSec = "10s";
         };
         wantedBy = [ "multi-user.target" ];
       };
 
+      ##################################################
       # Firewall v kontejneru
+      ##################################################
       networking.firewall.enable = true;
-      networking.firewall.allowedTCPPorts = [ 8443 ];
+      networking.firewall.allowedTCPPorts = [ 22 8443 ];
 
-      # SSH (volitelné)
-      services.openssh.enable = true;
-      services.openssh.settings.PermitRootLogin = "yes";
+      ##################################################
+      # SSH - minimalizovat konflikty
+      ##################################################
+      services.openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+          PermitRootLogin = "prohibit-password";
+        };
+      };
 
-      # Nastavit heslo pro root (pro SSH)
-      users.users.root.initialPassword = "changeme";
+      # Zakázat zbytečné služby, které mohou konfliktovat
+      services.resolved.enable = false;
+      services.timesyncd.enable = false;
+
+      ##################################################
+      # Základní balíčky
+      ##################################################
+      environment.systemPackages = with pkgs; [
+        vim htop curl wget git tmux docker-compose
+      ];
     };
   };
 }
--- containers/server2-dhcp.nix
+++ containers/server2-dhcp.nix
@@ -3,47 +3,83 @@
 {
   containers.server2 = {
     autoStart = true;
-    privateNetwork = false;  # Sdílet hostitelskou síť
+    privateNetwork = false;
+
+    extraFlags = [
+      "--bind=/tmp"
+      "--bind=/run"
+      "--bind=/sys/fs/cgroup"
+    ];
 
     config = { config, pkgs, lib, ... }: {
       networking.hostName = "server2";
       system.stateVersion = "25.05";
 
-      # Povolit DHCP v kontejneru
-      networking.useDHCP = true;
+      ##################################################
+      # ŘEŠENÍ KONFLIKTU
+      ##################################################
+      networking.networkmanager.enable = lib.mkForce false;
+      networking.useDHCP = lib.mkForce true;
 
+      networking.dhcpcd.enable = false;
+      networking.interfaces = {};
+      networking.defaultGateway = lib.mkForce null;
+      networking.nameservers = lib.mkForce [];
+
+      ##################################################
+      # Docker
+      ##################################################
       virtualisation.docker = {
         enable = true;
         enableOnBoot = true;
+        package = pkgs.docker_27;
+        daemon.settings = {
+          ip = "0.0.0.0";
+        };
       };
 
+      ##################################################
+      # Portainer Business Edition
+      ##################################################
       systemd.services.portainer = {
-        description = "Portainer Business Edition";
+        description = "Portainer Business Edition 2.33.6";
         after = [ "docker.service" ];
         requires = [ "docker.service" ];
         serviceConfig = {
-          ExecStart = ''
-            ${pkgs.docker}/bin/docker run \
-              --name portainer \
-              --restart unless-stopped \
-              -p 9443:9000 \
-              -v /var/run/docker.sock:/var/run/docker.sock \
-              -v portainer_data:/data \
-              portainer/portainer-ee:2.33.6
-          '';
-          ExecStop = "${pkgs.docker}/bin/docker stop portainer";
-          ExecStopPost = "${pkgs.docker}/bin/docker rm portainer";
-          Restart = "on-failure";
+          Type = "simple";
+          ExecStart = "${pkgs.docker}/bin/docker run \
+            --name portainer \
+            --restart=always \
+            -p 9443:9000 \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v portainer_data:/data \
+            portainer/portainer-ee:2.33.6";
+          Restart = "always";
+          RestartSec = "10s";
         };
         wantedBy = [ "multi-user.target" ];
       };
 
+      ##################################################
+      # Firewall
+      ##################################################
       networking.firewall.enable = true;
-      networking.firewall.allowedTCPPorts = [ 9443 ];
+      networking.firewall.allowedTCPPorts = [ 22 9443 ];
 
+      ##################################################
+      # SSH
+      ##################################################
       services.openssh.enable = true;
-      services.openssh.settings.PermitRootLogin = "yes";
-      users.users.root.initialPassword = "changeme";
+
+      services.resolved.enable = false;
+      services.timesyncd.enable = false;
+
+      ##################################################
+      # Základní balíčky
+      ##################################################
+      environment.systemPackages = with pkgs; [
+        vim htop curl wget git tmux docker-compose
+      ];
     };
   };
 }
