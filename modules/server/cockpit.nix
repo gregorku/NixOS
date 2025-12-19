c99dd61220f5b67b88cef609b47513409e4b3ec5
gregorku <gregorku@atlas.cz>
2025-12-19 05:11:29 +0100

    upgrade 19.12.2025-5

--- modules/server/cockpit.nix
+++ modules/server/cockpit.nix
@@ -3,12 +3,21 @@
 {
   services.cockpit = {
     enable = true;
-    openFirewall = true;
+    openFirewall = true; [cite: 1]
     settings = {
       WebService = {
         AllowUnencrypted = true;
+        # Toto pomůže, pokud prohlížeč blokuje mix HTTP/HTTPS
         ProtocolHeader = "X-Forwarded-Proto";
+      };
+      # Přidání této sekce může pomoci s přihlašovací smyčkou
+      Session = {
+        IdleTimeout = 15;
+        Banner = "/etc/issue";
       };
     };
   };
+
+  # Ujistěte se, že Cockpit má přístup k PAM
+  security.pam.services.cockpit = {};
 }
