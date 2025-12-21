5f1e068cc5a393d485e239f1ab318d32d54f7208
gregorku <gregorku@atlas.cz>
2025-12-21 16:00:32 +0100

    Configures Cockpit to listen on IPv4
    Configures the Cockpit service to explicitly listen on IPv4 addresses by defining the socket in NixOS style.
    This ensures that Cockpit binds to IPv4 address.

--- modules/server/cockpit.nix
+++ modules/server/cockpit.nix
@@ -4,16 +4,15 @@
   services.cockpit = {
     enable = true;
     port = 9090;
-    openFirewall = false;  # Firewall je vypnutý, ale pro úplnost
+    openFirewall = false;
+  };
 
-
-  # KLÍČOVÉ: Explicitně definovat socket v NixOS stylu
+  ## KLÍČOVÉ: Explicitně definovat socket (IPv4)
   systemd.sockets.cockpit = {
     enable = true;
     wantedBy = [ "sockets.target" ];
     socketConfig = {
-      # TOTO JE NEJDŮLEŽITĚJŠÍ - přinutí IPv4 naslouchání
       ListenStream = [ "0.0.0.0:9090" ];
-      # Pro IPv6 přidejte: "[::]:9090"
+    };
   };
 }
