26fe985b8d99d026bca56d0e7804a29eddb89911
gregorku <gregorku@atlas.cz>
2025-12-21 21:20:02 +0100

    Configures ha-doma container with macvlan networking
    Sets up the ha-doma container using NixOS containers.
    It configures the container to use macvlan networking
    bridged to an existing br0 interface on the host.
    It also enables DHCP for the container via systemd-networkd.

--- containers/ha-doma/container.nix
+++ containers/ha-doma/container.nix
@@ -2,17 +2,16 @@
 
 {
   containers.ha-doma = {
-    autoStart = true; # [cite: 5]
+    autoStart = true; [cite: 5]
 
-    # Konfigurace kontejneru
-    config = import ./default.nix; # [cite: 6]
+    # Import vnitřní konfigurace
+    config = import ./default.nix; [cite: 6]
 
     # SÍŤ – Propojení s existujícím br0 na hostiteli
     privateNetwork = true;
     macvlans = [ "br0" ];
 
-    # Povolení tunelu (pokud je potřeba pro HA/VPN)
-    enableTun = true; # [cite: 8]
-    enableTunTap = true; # [cite: 8]
+    # Povolení TUN/TAP zařízení
+    enableTun = true; [cite: 8]
   };
 }
--- containers/ha-doma/default.nix
+++ containers/ha-doma/default.nix
@@ -1,20 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  system.stateVersion = "24.05"; #
+  system.stateVersion = "24.05"; [cite: 1]
 
   # IDENTITA KONTEJNERU
-  networking.hostName = "ha-doma"; #
+  networking.hostName = "ha-doma"; [cite: 1]
 
-  # SÍŤ – DHCP z LAN přes macvlan (přemostěno na br0)
-  networking.useNetworkd = true; # [cite: 2]
-  systemd.network.enable = true; # [cite: 2]
+  # SÍŤ – DHCP z LAN přes virtuální rozhraní macvlan
+  networking.useNetworkd = true; [cite: 2]
+  systemd.network.enable = true; [cite: 2]
 
-  systemd.network.networks."mv-br0" = {
-    matchConfig.Name = "mv-br0";
-    networkConfig.DHCP = "yes"; # [cite: 3]
+  systemd.network.networks."10-macvlan" = {
+    matchConfig.Name = "mv-*";
+    networkConfig.DHCP = "yes"; [cite: 3]
   };
 
   # ZÁKLAD
-  services.openssh.enable = true; # [cite: 4]
+  services.openssh.enable = true; [cite: 4]
 }
