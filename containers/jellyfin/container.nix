7d6e0119c81973b02706d67c6d4140315de743f3
gregorku <gregorku@atlas.cz>
2025-12-23 21:10:12 +0100

    Adds user namespace mapping for Jellyfin
    Adds uid and gid mapping to allow the Jellyfin container to run as root inside the container, while mapping to a non-privileged user on the host.
    
    This improves compatibility with certain host configurations and avoids potential permission issues.

--- containers/jellyfin/container.nix
+++ containers/jellyfin/container.nix
@@ -9,6 +9,13 @@
     macvlans = [ "br0" ];
     enableTun = true;
 
+    uidMap = [
+      { hostID = 0; containerID = 0; size = 65536; }
+    ];
+    gidMap = [
+      { hostID = 0; containerID = 0; size = 65536; }
+    ];
+
     bindMounts = {
       "/data/media" = {
         hostPath = "/data/media";
