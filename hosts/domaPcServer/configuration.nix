ca63ab8130ed571c905df51db433a1eb8d46d030
gregorku <gregorku@atlas.cz>
2025-12-25 20:35:20 +0100

    Updates SSH secrets path
    Updates the path to the SSH secrets file to a more consistent location, improving maintainability and organization of secret files.

--- hosts/domaPcServer/configuration.nix
+++ hosts/domaPcServer/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    "${secretsPath}/ssh"
+    ../secrets/ssh
 
     ##################################################
     # Common – sdílené moduly
