a210fb9392da4ad63ecc5d2dc4a5b1520b4f1227
gregorku <gregorku@atlas.cz>
2025-12-25 20:41:07 +0100

    Updates SSH secret import path
    Updates the import path for the SSH secret to use the absolute path.
    This ensures that the SSH key is correctly imported, regardless of the current working directory.

--- hosts/domaPcServer/configuration.nix
+++ hosts/domaPcServer/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./secrets/ssh
+    /etc/nixos/secrets/ssh
 
     ##################################################
     # Common – sdílené moduly
