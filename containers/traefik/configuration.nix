97b00be2ff74009bc15ff4f6405102c97a9ea214
gregorku <gregorku@atlas.cz>
2025-12-25 15:56:00 +0100

    Deploys Traefik reverse proxy service
    Sets up Traefik as a reverse proxy, including configuration for
    TLS certificate resolution via Cloudflare, CrowdSec integration for
    security, and routing rules for Home Assistant and the Traefik
    dashboard. Configures static IP networking.

--- containers/traefik/configuration.nix
+++ containers/traefik/configuration.nix
@@ -1,18 +1,83 @@
 { config, pkgs, ... }:
 
 {
+  system.stateVersion = "24.05";
+
+  networking = {
+    hostName = "traefik";
+    useDHCP = false;
+
+    interfaces.eth0.ipv4.addresses = [{
+      address = "192.168.100.231";
+      prefixLength = 24;
+    }];
+
+    defaultGateway = "192.168.100.1";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    firewall.allowedTCPPorts = [ 443 8888 ];
+  };
+
   services.traefik = {
     enable = true;
-    environmentFiles = [ "/run/secrets/crowdsec/env" ];
+    environmentFiles = [ "/run/secrets/traefik/env" ];
 
-  staticConfigOptions.experimental.plugins.crowdsec = {
-    moduleName = "github.com/maxlerebourg/crowdsec-bouncer-traefik-plugin";
-    version = "v1.4.2";
-  };
+    staticConfigOptions = {
+      entryPoints = {
+        websecure.address = ":443";
+        admin.address = ":8888";
+      };
 
-  dynamicConfigOptions.http.middlewares.crowdsec.plugin.crowdsec = {
-    enabled = true;
-    crowdsecLapiKey = "$CROWDSEC_BOUNCER_API_KEY";
-    crowdsecLapiUrl = "http://192.168.100.1:8080";
+      api.dashboard = true;
+
+      certificatesResolvers.cloudflare.acme = {
+        email = "admin@homeassistant.serveftp.org";
+        storage = "/var/lib/traefik/acme.json";
+        dnsChallenge = {
+          provider = "cloudflare";
+          delayBeforeCheck = 10;
+        };
+      };
+
+      experimental.plugins.crowdsec = {
+        moduleName = "github.com/maxlerebourg/crowdsec-bouncer-traefik-plugin";
+        version = "v1.4.2";
+      };
+    };
+
+    dynamicConfigOptions.http = {
+      routers = {
+        ha = {
+          rule = "Host(`homeassistant.serveftp.org`)";
+          entryPoints = [ "websecure" ];
+          tls.certResolver = "cloudflare";
+          service = "ha";
+          middlewares = [ "crowdsec" ];
+        };
+
+        traefik = {
+          rule = "Host(`traefik.lan`)";
+          entryPoints = [ "admin" ];
+          service = "api@internal";
+          middlewares = [ "lan-only" ];
+        };
+      };
+
+      services.ha.loadBalancer.servers = [
+        { url = "http://192.168.100.230:8123"; }
+      ];
+
+      middlewares = {
+        lan-only.ipWhiteList.sourceRange = [
+          "192.168.100.0/24"
+          "120.100.100.0/24"
+        ];
+
+        crowdsec.plugin.crowdsec = {
+          enabled = true;
+          crowdsecLapiKey = "$CROWDSEC_BOUNCER_API_KEY";
+          crowdsecLapiUrl = "http://192.168.100.1:8080";
+        };
+      };
+    };
   };
-};
+}
