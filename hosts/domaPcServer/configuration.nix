0826f79fd7eb3e01f36184827e69867110f747f5
gregorku <gregorku@atlas.cz>
2025-12-25 13:37:48 +0100

    Updates ssh secrets import path
    Uses a variable for the ssh secrets import path,
    to improve maintainability and flexibility.

--- hosts/domaPcServer/configuration.nix
+++ hosts/domaPcServer/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./secrets/ssh
+    "${secretsPath}/ssh"
 
     ##################################################
     # Common – sdílené moduly
