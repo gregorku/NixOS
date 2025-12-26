b02ceeb2940575763d4c27f180abd7613dbe0797
gregorku <gregorku@atlas.cz>
2025-12-26 17:02:54 +0100

    Configures Caddy container with static IP
    Configures the Caddy container with a static IP address.
    
    This allows for a predictable and reliable network configuration,
    especially when reverse proxying to other services. It also sets up
    basic firewall rules to allow access to the Caddy instance. The
    previous DHCP configuration is removed.

--- containers/caddy/configuration.nix
+++ containers/caddy/configuration.nix
@@ -1,12 +1,20 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
   system.stateVersion = "25.05";
-  networking.hostName = "caddy";
 
-  systemd.network.networks."10-macvlan" = {
-    matchConfig.Name = "mv-*";
-    networkConfig.DHCP = "yes";
+  networking = {
+    hostName = "caddy";
+    useDHCP = false;
+
+    interfaces.eth0.ipv4.addresses = [{
+      address = "192.168.100.232";
+      prefixLength = 24;
+    }];
+
+    defaultGateway = "192.168.100.1";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    firewall.allowedTCPPorts = [ 443 ];
   };
 
   services.caddy = {
