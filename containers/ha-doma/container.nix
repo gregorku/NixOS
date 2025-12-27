8b6be275cf8ba99bda3b70edca6cd38cad1238f7
gregorku <gregorku@atlas.cz>
2025-12-27 03:12:51 +0100

    Přidána podpora pro externí soubor s tajemstvími v konfiguraci Zigbee2mqtt

--- containers/ha-doma/container.nix
+++ containers/ha-doma/container.nix
@@ -49,15 +49,19 @@
       "/data" = {
         hostPath = "/data/zigbee2mqtt";
         isReadOnly = false;
-      };
+
+      "/data/mqtt-secrets.yaml" = {
+        hostPath = "/etc/nixos/secrets/zigbee2mqtt/mqtt-secret.yaml";
+        isReadOnly = true;
 
       "/dev/zigbee" = {
         hostPath = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
         isReadOnly = false;
+        };
       };
     };
-
     config = ./zigbee2mqtt-default.nix;
+    };
   };
 }
 
