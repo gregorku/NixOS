202c1f13af9d3a901544777d9fba0b3b3719803b
gregorku <gregorku@atlas.cz>
2025-12-27 20:01:12 +0100

    Odstraněna konfigurace kontejneru Zigbee2mqtt pro zjednodušení a údržbu

--- containers/ha-doma/container.nix
+++ containers/ha-doma/container.nix
@@ -31,37 +31,5 @@
     };
     config = import ./postgres-default.nix;
   };
-
-  containers.zigbee2mqtt = {
-    autoStart = true;
-    privateNetwork = true;
-    macvlans = [ "br0" ];
-    privateUsers = "identity";
-
-    allowedDevices = [
-      {
-        node = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
-        modifier = "rw";
-      }
-    ];
-
-    bindMounts = {
-      "/data" = {
-        hostPath = "/data/zigbee2mqtt";
-        isReadOnly = false;
-
-      "/data/mqtt-secrets.yaml" = {
-        hostPath = "/etc/nixos/secrets/zigbee2mqtt/mqtt-secret.yaml";
-        isReadOnly = true;
-
-      "/dev/zigbee" = {
-        hostPath = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
-        isReadOnly = false;
-        };
-      };
-    };
-    config = ./zigbee2mqtt-default.nix;
-    };
-  };
 }
 
