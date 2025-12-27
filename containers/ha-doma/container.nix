afbc426f87989cd3c96ecd39efac6753ccc1fe41
gregorku <gregorku@atlas.cz>
2025-12-27 02:32:59 +0100

    Přidána konfigurace pro službu Zigbee2mqtt do souboru container.nix a vytvořen nový konfigurační soubor zigbee2mqtt-default.nix

--- containers/ha-doma/container.nix
+++ containers/ha-doma/container.nix
@@ -8,20 +8,9 @@
     enableTun = true;
     privateUsers = "identity";
 
-    allowedDevices = [
-      {
-        node = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
-        modifier = "rw";
-      }
-    ];
-
     bindMounts = {
       "/config" = {
         hostPath = "/data/homeassistant";
-        isReadOnly = false;
-      };
-      "/dev/zigbee" = {
-        hostPath = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
         isReadOnly = false;
       };
     };
@@ -42,4 +31,33 @@
     };
     config = import ./postgres-default.nix;
   };
+
+  containers.zigbee2mqtt = {
+    autoStart = true;
+    privateNetwork = true;
+    macvlans = [ "br0" ];
+    privateUsers = "identity";
+
+    allowedDevices = [
+      {
+        node = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
+        modifier = "rw";
+      }
+    ];
+
+    bindMounts = {
+      "/data" = {
+        hostPath = "/data/zigbee2mqtt";
+        isReadOnly = false;
+      };
+
+      "/dev/zigbee" = {
+        hostPath = "/dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-07Mg24_0ab50f4025adef1196c58a4ba8793231-if00-port0";
+        isReadOnly = false;
+      };
+    };
+
+    config = ./zigbee2mqtt-default.nix;
+  };
 }
+
