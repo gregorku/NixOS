5d2baaab9cda607cb12ededd4ca3b5cf94cc39cb
gregorku <gregorku@atlas.cz>
2025-12-28 20:36:01 +0100

    fregate

--- containers/frigate/container.nix
+++ containers/frigate/container.nix
@@ -8,8 +8,6 @@
     privateNetwork = true;
     macvlans = [ "br0" ];
     enableTun = true;
-
-    # Stejné UID/GID jako hostitel – bez permission pekla
     privateUsers = "identity";
 
     bindMounts = {
--- containers/frigate/frigate-default.nix
+++ containers/frigate/frigate-default.nix
@@ -1,16 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  ## =========================================================
-  ## SÍŤOVÁ KONFIGURACE – beze změn
-  ## =========================================================
   networking.useDHCP = false;
   networking.useNetworkd = true;
   networking.useHostResolvConf = false;
   services.resolved.enable = true;
-
-  systemd.targets.multi-user.enable = true;
-  systemd.defaultUnit = "multi-user.target";
 
   systemd.network.enable = true;
   systemd.network.networks."10-macvlan" = {
@@ -19,42 +13,34 @@
     dhcpV4Config.ClientIdentifier = "mac";
   };
 
-  # Frigate UI + RTMP
   networking.firewall.allowedTCPPorts = [ 5000 1935 ];
 
   system.stateVersion = "25.11";
 
-  ## ---------------------------------------------------------
-  ## Uživatel frigate
-  ## ---------------------------------------------------------
   users.groups.frigate = {};
   users.users.frigate = {
     isSystemUser = true;
     group = "frigate";
+    home = "/data/frigate";
   };
 
-  ## =========================================================
-  ## FRIGATE – RUČNÍ SYSTEMD SERVICE
-  ## =========================================================
-  environment.systemPackages = with pkgs; [
-    frigate
-    ffmpeg
-  ];
+  environment.systemPackages = with pkgs; [ frigate ffmpeg ];
 
-  ssystemd.services.frigate = {
-  description = "Frigate NVR";
+  systemd.targets.multi-user.enable = true;
+  systemd.defaultUnit = "multi-user.target";
 
-  wantedBy = [ "multi-user.target" ];
+  systemd.services.frigate = {
+    description = "Frigate NVR";
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
 
-  wants = [ "network-online.target" ];
-  after  = [ "network-online.target" ];
-
-  serviceConfig = {
-    ExecStart = "${pkgs.frigate}/bin/frigate -c /data/frigate/config/config.yml";
-    Restart = "always";
-    User = "frigate";
-    Group = "frigate";
-    WorkingDirectory = "/data/frigate";
+    serviceConfig = {
+      ExecStart = "${pkgs.frigate}/bin/frigate -c /data/frigate/config/config.yml";
+      Restart = "always";
+      User = "frigate";
+      Group = "frigate";
+      WorkingDirectory = "/data/frigate";
     };
   };
 }
