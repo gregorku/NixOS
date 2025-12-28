1dca740c58cc9a9307f3e0ee9ff75c6e4d9c7865
gregorku <gregorku@atlas.cz>
2025-12-28 11:26:44 +0100

    ha

--- flake.nix
+++ flake.nix
@@ -3,12 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    # Stabilní balíky pro problematické služby (Home Assistant apod.)
+    nixpkgs2411.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, nixpkgs2411, ... }:
   let
     mkHost = host: nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+
+      specialArgs = {
+        pkgs2411 = import nixpkgs2411 {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
+
       modules = [
         ./hosts/${host}/configuration.nix
       ];
@@ -22,3 +33,4 @@
     };
   };
 }
+
