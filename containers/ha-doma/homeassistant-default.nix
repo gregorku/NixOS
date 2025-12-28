b814a8071abc6c81d742e890fe066002edfa7dee
gregorku <gregorku@atlas.cz>
2025-12-28 15:43:45 +0100

    ha

--- containers/ha-doma/homeassistant-default.nix
+++ containers/ha-doma/homeassistant-default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.11";
   networking.hostName = "homeassistant";
 
   networking.useNetworkd = true;
@@ -14,22 +14,65 @@
     networkConfig.DHCP = "yes";
   };
 
+  virtualisation.containers.containersConf.settings = {
+    containers.keyring = false;
+  };
+
+  environment.systemPackages = with pkgs; [
+    mc
+  ];
+  ## =========================
+  ## FIREWALL
+  ## =========================
+  networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [ 8123 ];
 
+  # ESPHome potřebuje mDNS (Avahi) pro vyhledávání senzorů v síti
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      userServices = true;
+    };
+  };
+
   users.users.homeassistant = {
+    uid = 911;
     isSystemUser = true;
     group = "homeassistant";
+    extraGroups = [ "dialout" ];
   };
-  users.groups.homeassistant = {};
+  users.groups.homeassistant.gid = 911;
 
   services.home-assistant = {
     enable = true;
     configDir = "/config";
 
-    extraPackages = python3Packages: with python3Packages; [
-      paho-mqtt
-      aioesphomeapi
-      psycopg2
+    config = import ./configuration.nix;
+
+    extraComponents = [
+      "hardware"
+      "usb"
+      "bluetooth"
+      "met"
+      "esphome" # Explicitně přidáno
+      "zeroconf" # Nutné pro mDNS/ESPHome vyhledávání
+     ];
+
+  extraPackages = python3Packages: [
+      python3Packages.psycopg2
+      python3Packages.gtts
+      python3Packages.pymetno
+      python3Packages.home-assistant-chip-clusters
+      python3Packages.universal-silabs-flasher
+      python3Packages.zha-quirks             # Knihovna pro specifická zařízení
+      python3Packages.zha                    # Samotná knihovna ZHA
+      python3Packages.zigpy-znp              # Pokud byste někdy přešel na TI čip (pro jistotu)
+      python3Packages.zigpy-deconz           # Pro podporu různých adaptérů
+      python3Packages.bellows                # Protokol pro Silicon Labs (EZSP)
+      python3Packages.zigpy                  # Základní knihovna pro Zigbee
     ];
   };
 }
