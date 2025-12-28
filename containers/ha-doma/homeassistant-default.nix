aec7a61401d0bcdb6477a30ad0776c7cbbe644e5
gregorku <gregorku@atlas.cz>
2025-12-28 15:22:39 +0100

    ha

--- containers/ha-doma/homeassistant-default.nix
+++ containers/ha-doma/homeassistant-default.nix
@@ -1,5 +1,14 @@
-{ config, pkgs, pkgs2411, ... }:
+{ config, pkgs, ... }:
 
+let
+  # Stabilní nixpkgs 24.11 pouze pro Home Assistant
+  pkgs2411 = import (builtins.fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-24.11.tar.gz"
+  ) {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+  };
+in
 {
   system.stateVersion = "25.11";
   networking.hostName = "homeassistant";
@@ -14,29 +23,29 @@
     networkConfig.DHCP = "yes";
   };
 
-  virtualisation.containers.containersConf.settings = {
-    containers.keyring = false;
-  };
+  virtualisation.containers.containersConf.settings.containers.keyring = false;
 
   environment.systemPackages = with pkgs; [
     mc
   ];
+
   ## =========================
   ## FIREWALL
   ## =========================
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [ 8123 ];
 
+  users.groups.homeassistant.gid = 911;
   users.users.homeassistant = {
     uid = 911;
     isSystemUser = true;
     group = "homeassistant";
     extraGroups = [ "dialout" ];
   };
-  users.groups.homeassistant.gid = 911;
 
   services.home-assistant = {
     enable = true;
+    package = pkgs2411.home-assistant;
     configDir = "/config";
 
     config = import ./configuration.nix;
@@ -46,20 +55,22 @@
       "usb"
       "bluetooth"
       "met"
-     ];
+    ];
 
-  extraPackages = python3Packages: [
-      python3Packages.psycopg2
-      python3Packages.gtts
-      python3Packages.pymetno
-      python3Packages.home-assistant-chip-clusters
-      python3Packages.universal-silabs-flasher
-      python3Packages.zha-quirks             # Knihovna pro specifická zařízení
-      python3Packages.zha                    # Samotná knihovna ZHA
-      python3Packages.zigpy-znp              # Pokud byste někdy přešel na TI čip (pro jistotu)
-      python3Packages.zigpy-deconz           # Pro podporu různých adaptérů
-      python3Packages.bellows                # Protokol pro Silicon Labs (EZSP)
-      python3Packages.zigpy                  # Základní knihovna pro Zigbee
+    extraPackages = python3Packages: with python3Packages; [
+      paho-mqtt
+      aioesphomeapi
+      psycopg2
+      gtts
+      pymetno
+      home-assistant-chip-clusters
+      universal-silabs-flasher
+      zha-quirks
+      zha
+      zigpy-znp
+      zigpy-deconz
+      bellows
+      zigpy
     ];
   };
 }
