f237c5a2115eddf26616676d4f730d635a90b52b
gregorku <gregorku@atlas.cz>
2025-12-28 11:51:05 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -2,31 +2,14 @@
   description = "NixOS configuration for multiple devices";
 
   inputs = {
-    # Hlavní větev – 25.11
-    nixpkgs.url = "tarball+https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-25.11.tar.gz";
-
-    # Stabilní balíky – 24.11 (Home Assistant, MQTT, ESPHome…)
-    nixpkgs2411.url = "tarball+https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-24.11.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, nixpkgs2411, ... }:
+  outputs = { self, nixpkgs, ... }:
   let
-    system = "x86_64-linux";
-
-    pkgs2411 = import nixpkgs2411 {
-      inherit system;
-      config.allowUnfree = true;
-    };
-
     mkHost = host: nixpkgs.lib.nixosSystem {
-      inherit system;
-
+      system = "x86_64-linux";
       modules = [
-        # 🔑 ZDE SE PKGS2411 VLOŽÍ DO _module.args
-        ({ ... }: {
-          _module.args.pkgs2411 = pkgs2411;
-        })
-
         ./hosts/${host}/configuration.nix
       ];
     };
