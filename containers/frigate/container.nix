024a361a360e47567d87e82b42af67b169ddb151
gregorku <gregorku@atlas.cz>
2025-12-29 21:38:03 +0100

    frigate

--- containers/frigate/container.nix
+++ containers/frigate/container.nix
@@ -3,28 +3,42 @@
 {
   containers.frigate = {
     autoStart = true;
-    # ... vaše ostatní nastavení ...
-
-    config = { config, pkgs, ... }: {
-      # Instalace knihovny i uvnitř kontejneru
-      environment.systemPackages = [ pkgs.libedgetpu ];
-      
-      # Frigate konfigurace (zjednodušeně)
-      services.frigate = {
-        enable = true;
-        settings = {
-          detectors.coral = {
-            type = "edgetpu";
-            device = "usb"; # Frigate si ho najde přes USB sběrnici
-          };
-          # ... zbytek frigate.yml ...
-        };
-      };
-    };
-
-    # KLÍČOVÉ: Povolení přístupu k hardwaru z hostitele do kontejneru
+    
+    # KLÍČOVÉ: Propojení USB z hostitele do kontejneru
+    # Toto umožní kontejneru vidět Coral i předtím, než vznikne /dev/apex_0
     bindMounts = {
       "/dev/bus/usb" = { hostPath = "/dev/bus/usb"; isReadOnly = false; };
+    };
+
+    config = { config, pkgs, ... }: {
+      # Povolení unfree balíčků uvnitř kontejneru (pokud je třeba pro Coral/GPU)
+      nixpkgs.config.allowUnfree = true;
+
+      services.frigate = {
+        enable = true;
+        
+        # OPRAVA CHYBY: Hostname je povinný
+        hostname = "frigate.doma.lan";
+
+        settings = {
+          # Detektory - definice Coralu
+          detectors.coral = {
+            type = "edgetpu";
+            device = "usb";
+          };
+
+          # Zde pokračuje vaše stávající konfigurace (kamery, atd.)
+          # cameras = { ... };
+        };
+      };
+
+      # Instalace knihoven pro Coral i uvnitř kontejneru
+      environment.systemPackages = [ pkgs.libedgetpu ];
+
+      # Firewall v kontejneru (pokud ho máte aktivní)
+      networking.firewall.allowedTCPPorts = [ 5000 1935 ];
+      
+      system.stateVersion = "25.11";
     };
   };
 }
