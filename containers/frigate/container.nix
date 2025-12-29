620bac8a63c843b1da96672469a31b3d359c4353
gregorku <gregorku@atlas.cz>
2025-12-29 21:42:23 +0100

    fr

--- containers/frigate/container.nix
+++ containers/frigate/container.nix
@@ -4,40 +4,35 @@
   containers.frigate = {
     autoStart = true;
     
-    # Propojení USB sběrnice, aby kontejner mohl inicializovat Coral
+    # 1. Povolení surového přístupu k hardwaru pro nspawn
+    additionalCapabilities = [ "CAP_SYS_RAWIO" ];
+
+    # 2. Mapování USB sběrnice (ponecháme)
     bindMounts = {
       "/dev/bus/usb" = { hostPath = "/dev/bus/usb"; isReadOnly = false; };
     };
 
-    config = { config, pkgs, ... }: {
-      # Nutné pro libedgetpu a případné ovladače
-      nixpkgs.config.allowUnfree = true;
+    # 3. Povolení přístupu k USB zařízením v cgroups
+    extraFlags = [ "--property=DeviceAllow=char-usb_device rwm" ];
 
+    config = { config, pkgs, ... }: {
+      # ... vaše stávající config.services.frigate ...
+      # Ujistěte se, že uvnitř zůstává:
+      nixpkgs.config.allowUnfree = true;
       services.frigate = {
         enable = true;
-        hostname = "frigate.doma.lan"; # Povinné
-        
+        hostname = "frigate.doma.lan";
         settings = {
-          # Detektor Coral
           detectors.coral = {
             type = "edgetpu";
             device = "usb";
           };
-
-          # OPRAVA CHYBY: Musí zde být definována alespoň jedna (třeba i vypnutá) kamera
           cameras.test_camera = {
             enabled = false;
-            ffmpeg.inputs = [{
-              path = "rtsp://127.0.0.1:554/live";
-              roles = [ "detect" ];
-            }];
+            ffmpeg.inputs = [{ path = "rtsp://127.0.0.1:554/live"; roles = [ "detect" ]; }];
           };
-
-          # Minimální nutná konfigurace pro start
-          mqtt.enabled = false; 
         };
       };
-
       environment.systemPackages = [ pkgs.libedgetpu ];
       system.stateVersion = "25.11";
     };
