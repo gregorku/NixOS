9af9dc2f3d28a44486d66e36482def6159bfd7f7
gregorku <108758519+gregorku@users.noreply.github.com>
2026-03-15 11:22:36 +0000

    Format and organize power management settings

--- modules/notebook-power.nix
+++ modules/notebook-power.nix
@@ -9,14 +9,14 @@
   # Preferuj úspornější chování na baterii
   powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
 
-# Wi-Fi / Bluetooth power saving
-# --------------------------------------------------
-networking.networkmanager.wifi.powersave = true;
+  # Wi-Fi / Bluetooth power saving
+  # --------------------------------------------------
+  networking.networkmanager.wifi.powersave = true;
 
-hardware.bluetooth = {
-  enable = true;
-  powerOnBoot = lib.mkDefault false;
-};
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = lib.mkDefault false;
+  };
 
   # --------------------------------------------------
   # Touchpad (libinput)
