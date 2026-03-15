3a1189762db4c630814786d8a4a1ae47e1c2940f
gregorku <108758519+gregorku@users.noreply.github.com>
2026-03-15 14:44:11 +0000

    Update device identifier for DataLinux filesystem

--- hosts/ntbLenovo/configuration.nix
+++ hosts/ntbLenovo/configuration.nix
@@ -50,7 +50,7 @@
   };
   #disk DataLinux
   fileSystems."/mnt/DataLinux" = {
-  device = "/dev/disk/by-label/DataLinux";
+  device = "/dev/disk/by-uuid/b38e75c9-a885-4713-aa6f-d5ea8a0fde1a";
   fsType = "btrfs";
   options = [
     "compress=zstd"
