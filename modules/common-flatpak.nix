951591b8d74961c43c27696f62d01363b444de36
Gregor <gregorku@atlas.cz>
2026-03-18 20:12:03 +0100

    flatpak novy

--- modules/common-flatpak.nix
+++ modules/common-flatpak.nix
@@ -1,44 +1,34 @@
 { config, pkgs, ... }:
 
-{
-  services.flatpak.enable = true;
-
-  # ----------------------
-  # Přidání Flathub repo
-  # ----------------------
-  systemd.services.flatpak-repo = {
-    description = "Add Flathub repository";
-
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-
-    serviceConfig.Type = "oneshot";
-    
-    # Použijeme 'script', který NixOS spustí v shellu
-    script = ''
-      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
+let
+  # Definice modulu (stáhne se automaticky z GitHubu)
+  nix-flatpak = builtins.fetchTarball {
+    url = "https://github.com/gmodena/nix-flatpak/archive/main.tar.gz";
   };
+in
+{
+  imports = [ nix-flatpak ];
 
-  # ----------------------
-  # Instalace aplikací
-  # ----------------------
-  systemd.services.flatpak-install = {
-    description = "Install Flatpak apps";
+  services.flatpak = {
+    enable = true;
 
-    wantedBy = [ "multi-user.target" ];
-    after = [
-      "flatpak-repo.service"
-      "network-online.target"
+    # Automaticky přidá Flathub repo (nemusíš psát systemd službu)
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }
     ];
-    wants = [ "network-online.target" ];
 
-    serviceConfig.Type = "oneshot";
-    
-    # Opět čistší zápis přes 'script' místo volání bashe v ExecStart
-    script = ''
-      ${pkgs.flatpak}/bin/flatpak install -y --noninteractive flathub nz.mega.MEGAsync || true
-    '';
+    # Deklarativní seznam aplikací (žádné ExecStart ani bash skripty)
+    packages = [
+      "flathub:nz.mega.MEGAsync"
+    ];
+
+    # Volitelné: Automatické aktualizace
+    update.auto = {
+      enable = true;
+      onCalendar = "weekly";
+    };
   };
 }
