fae22f4a1b92a28aa09aa034ce9d13d9838456be
Gregor <gregorku@atlas.cz>
2026-03-18 18:43:57 +0100

    oprava mys

--- modules/notebook-power.nix
+++ modules/notebook-power.nix
@@ -14,14 +14,23 @@
   powerManagement.cpuFreqGovernor = "powersave";
 
   services.tlp.settings = {
+    # CPU výkon (tiché chování)
     CPU_MAX_PERF_ON_AC = 60;
     CPU_MAX_PERF_ON_BAT = 40;
 
     CPU_BOOST_ON_AC = 0;
     CPU_BOOST_ON_BAT = 0;
 
+    # USB power saving (bez hacků)
     USB_AUTOSUSPEND = 1;
 
+    # Lenovo platform profile (velmi důležité pro hluk)
+    PLATFORM_PROFILE_ON_AC = "low-power";
+    PLATFORM_PROFILE_ON_BAT = "low-power";
+
+    # jemné doladění CPU idle
+    SCHED_POWERSAVE_ON_AC = 1;
+    SCHED_POWERSAVE_ON_BAT = 1;
   };
 
   # --------------------------------------------------
