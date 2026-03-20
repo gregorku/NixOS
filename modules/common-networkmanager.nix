38baf400ee59b9728a07408351b45d037967e85b
Gregor <gregorku@atlas.cz>
2026-03-20 19:50:06 +0100

    vpn

--- modules/common-networkmanager.nix
+++ modules/common-networkmanager.nix
@@ -19,13 +19,23 @@
   };
 
   # ----------------------
-  # Síťové nástroje
+  # Síťové nástroje a VPN
   # ----------------------
   environment.systemPackages = with pkgs; [
     networkmanager
     wireguard-tools
     avahi
+    # Přidáno pro tvou pracovní VPN:
+    openconnect
+    vpn-slice
   ];
+
+  # ----------------------
+  # Zkratky pro terminál (Aliasy)
+  # ----------------------
+  programs.bash.shellAliases = {
+    vpn-work = "sudo openconnect --user=kutik --authgroup=UADFD01-ST-2FA --servercert pin-sha256:Myb+eKrw7BcomYOUYcpUvpfhLaZ84nQDygatExjB44U= --mtu 1200 --script='vpn-slice --no-host-names --no-ns-hosts --nbns 10.0.0.0/8' u.ivpn.cz";
+  };
 
   # ----------------------
   # Uživatel může spravovat síť
