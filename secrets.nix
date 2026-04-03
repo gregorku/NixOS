1fe8fc033e37dbf0edb3e81aea5e114bb342fde0
Gregor <gregorku@atlas.cz>
2026-04-03 10:32:54 +0200

    key age

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,8 @@
 let
-  serverVPStest = "age1ws5gtnuuamhksc8urq7rzekw3mrs968all0vkpvukc06dc4q7v0q7q58jr";  # sem vlož výstup
+  serverVPStest = "age1ws5gtnuuamhksc8urq7rzekw3mrs968all0vkpvukc06dc4q7v0q7q58jr";
+  ntbLenovo = "age1h85f8wc9sx5p5sep7h49qjgq3xvj9llwgz75fe0eep9ue6qt4f0s8djvs2";  # sem vlož výstup
 in
 {
-  "serverVPStest/test-secret.age".publicKeys = [ serverVPStest ];
+  "serverVPStest/test-secret.age".publicKeys = [ serverVPStest
+  ntbLenovo ];
 }
