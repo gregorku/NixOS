f9d2e2207827cca9ce56ec0fcba7bf97982bfea4
Gregor <gregorku@atlas.cz>
2026-04-05 15:25:53 +0200

    arm

--- modules/common-waydroid.nix
+++ modules/common-waydroid.nix
@@ -1,20 +1,31 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Waydroid
   virtualisation.waydroid.enable = true;
 
+  # Binder (nutné pro Android container)
   boot.kernelModules = [ "binder_linux" ];
 
+  # Grafika (nová volba místo opengl)
   hardware.graphics.enable = true;
 
+  # Audio (pokud používáš PipeWire)
   services.pipewire.enable = true;
 
+  # ARM translation (klíčová věc pro APK)
+  environment.sessionVariables = {
+    WAYDROID_ENABLE_ARM_TRANSLATION = "1";
+  };
+
+  # Uživatel
   users.users.gregor.extraGroups = [
     "waydroid"
     "video"
     "render"
   ];
 
+  # Balíčky
   environment.systemPackages = with pkgs; [
     waydroid
     android-tools
