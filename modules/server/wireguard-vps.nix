9ef93b7159be9200648f8d859c7d0468d98bf41e
Gregor <gregorku@atlas.cz>
2026-04-06 20:08:07 +0200

    wg

--- modules/server/wireguard-vps.nix
+++ modules/server/wireguard-vps.nix
@@ -7,6 +7,12 @@
   age.secrets.wg1_serverVPS-private.file =
     ../../VPSsecret/wireguard/wg1_serverVPS-private.age;
 
+  age.secrets.wg2_serverVPS-private.file =
+    ../../VPSsecret/wireguard/wg2_serverVPS-private.age;
+
+  age.secrets.wg3_serverVPS-private.file =
+    ../../VPSsecret/wireguard/wg3_serverVPS-private.age;
+
   ##################################################
   # WireGuard
   ##################################################
@@ -20,8 +26,36 @@
       peers = [
         {
           # Mikrotik doma wg1
-          publicKey = "ZRRN9IVqc8atE1Cby4k00YKe0bd74N7/TkKZkIybKyk=";
+          publicKey = "ZRRN9IVqc8atE1Cby4k00YKe0bd74N7/bscvkIybKyk=";
           allowedIPs = [ "10.100.100.100/32" ];
+        }
+      ];
+    };
+
+    wg2 = {
+      ips = [ "10.110.100.1/24" ];
+      listenPort = 51821;
+      privateKeyFile = config.age.secrets.wg2_serverVPS-private.path;
+
+      peers = [
+        {
+          # (doplníš peer wg2)
+          publicKey = "";
+          allowedIPs = [ ];
+        }
+      ];
+    };
+
+    wg3 = {
+      ips = [ "10.120.100.1/24" ];
+      listenPort = 51822;
+      privateKeyFile = config.age.secrets.wg3_serverVPS-private.path;
+
+      peers = [
+        {
+          # (doplníš peer wg3)
+          publicKey = "";
+          allowedIPs = [ ];
         }
       ];
     };
