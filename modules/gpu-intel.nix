1f82b26e171265175a66ec068e6325546bab8cce
Gregor <gregorku@atlas.cz>
2026-04-11 19:07:53 +0200

    gpu

--- modules/gpu-intel.nix
+++ modules/gpu-intel.nix
@@ -1,18 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  # Modern Intel iGPU setup (X11 + Wayland)
   hardware.graphics.enable = true;
 
-  # VAAPI / VDPAU
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
     intel-vaapi-driver
-    vaapiVdpau
+    libva-vdpau-driver
     libvdpau-va-gl
   ];
 
-  # Diagnostic / testing tools
   environment.systemPackages = with pkgs; [
     vulkan-tools
   ];
