e05f2849faadab17d238e0db06641a7d262262aa
gregor <gregorku@atlas.cz>
2026-04-20 19:23:14 +0200

    avahi

--- modules/common-networkmanager.nix
+++ modules/common-networkmanager.nix
@@ -1,30 +1,19 @@
-{ config, pkgs, lib, ... }:
-
 let
   wifiIfaces =
     builtins.filter (iface: lib.hasPrefix "wlp" iface)
     (builtins.attrNames config.networking.interfaces);
+
+  wifi =
+    if wifiIfaces == []
+    then [ "wlp2s0" "wlp4s0" ]  # fallback pro tvoje stroje
+    else wifiIfaces;
 in
 {
-  networking.networkmanager.enable = true;
-  networking.useNetworkd = false;
-
   services.avahi = {
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
     reflector = true;
-    allowInterfaces = wifiIfaces ++ [ "incusbr0" ];
+    allowInterfaces = wifi ++ [ "incusbr0" ];
   };
-
-  environment.systemPackages = with pkgs; [
-    networkmanager
-    wireguard-tools
-    psmisc
-    avahi
-    openconnect
-    vpn-slice
-  ];
-
-  users.users.gregor.extraGroups = [ "networkmanager" ];
 }
