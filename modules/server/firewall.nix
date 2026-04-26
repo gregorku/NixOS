18714184b84f8e59a089c706235b31d63c7c86d2
Gregor <gregorku@atlas.cz>
2026-04-26 18:17:24 +0200

    firewall

--- modules/server/firewall.nix
+++ modules/server/firewall.nix
@@ -1,34 +1,26 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Použití nftables místo starého iptables
+  # Použití nftables místo iptables
   networking.nftables.enable = true;
-  networking.firewall.enable = false; # vlastní ruleset
+  networking.firewall.enable = false;
 
   networking.nftables.ruleset = ''
     table inet filter {
 
-      #------------------------------------------------------------------
-      # Sady (sets) pro správu IP adres
-      #------------------------------------------------------------------
-      set trusted_ips {
-        type ipv4_addr
-        flags interval
-        elements = {
-          # 1.2.3.4/32,
-        }
-      }
-
-      #------------------------------------------------------------------
+      #--------------------------------------------------
       # INPUT – příchozí provoz
-      #------------------------------------------------------------------
+      #--------------------------------------------------
       chain input {
         type filter hook input priority filter; policy drop;
 
+        # Navázaná spojení
         ct state established,related accept
+
+        # Loopback
         iifname lo accept
 
-        # ICMP
+        # ICMP (ping)
         ip protocol icmp accept
         ip6 nexthdr icmpv6 accept
 
@@ -45,21 +37,22 @@
         udp dport 51820 accept
 
         # Cockpit
-        tcp dport 9090 ip saddr @trusted_ips accept
+        tcp dport 9090 accept
 
         # HAProxy stats
-        tcp dport 8404 ip saddr @trusted_ips accept
+        tcp dport 8404 accept
 
         # HAProxy TCP
         tcp dport 8443 accept
 
+        # Log + drop
         log prefix "DROPPED INPUT: " flags all limit rate 5/minute
         drop
       }
 
-      #------------------------------------------------------------------
+      #--------------------------------------------------
       # FORWARD – provoz přes server (Incus)
-      #------------------------------------------------------------------
+      #--------------------------------------------------
       chain forward {
         type filter hook forward priority filter; policy drop;
 
@@ -68,31 +61,31 @@
         # Incus → ven
         iifname "incusbr0" oifname != "incusbr0" accept
 
-        # ven → Incus (jen odpovědi)
+        # ven → Incus (odpovědi)
         iifname != "incusbr0" oifname "incusbr0" ct state established,related accept
 
         # WireGuard
         iifname "wg0" accept
         oifname "wg0" accept
 
-        # DNAT (port forwarding)
+        # DNAT
         ct status dnat accept
 
         log prefix "DROPPED FORWARD: " flags all limit rate 5/minute
         drop
       }
 
-      #------------------------------------------------------------------
+      #--------------------------------------------------
       # OUTPUT – odchozí provoz
-      #------------------------------------------------------------------
+      #--------------------------------------------------
       chain output {
         type filter hook output priority filter; policy accept;
       }
     }
 
-    #------------------------------------------------------------------
+    #--------------------------------------------------
     # NAT
-    #------------------------------------------------------------------
+    #--------------------------------------------------
     table inet nat {
 
       chain prerouting {
@@ -105,10 +98,10 @@
       chain postrouting {
         type nat hook postrouting priority srcnat; policy accept;
 
-        # Incus NAT (bez závislosti na eth0!)
+        # NAT pro Incus kontejnery
         ip saddr 10.10.10.0/24 oifname != "incusbr0" masquerade
 
-        # WireGuard NAT
+        # NAT pro WireGuard
         ip saddr 10.100.0.0/24 oifname != "wg0" masquerade
       }
     }
