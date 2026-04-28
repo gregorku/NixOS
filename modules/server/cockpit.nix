5d6e7a83967af7705da0bdee661c888856fa169b
Gregor <gregorku@atlas.cz>
2026-04-28 20:36:09 +0200

    cockpit

--- modules/server/cockpit.nix
+++ modules/server/cockpit.nix
@@ -1,14 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  # 1. POVOLENÍ PCP
-  services.pcp = {
-    enable = true;
-    # režim "standalone" = pmcd + pmlogger + pmie + pmproxy [citation:1]
-    presets = "standalone"; 
-  };
-
-  # 2. KONFIGURACE COCKPITU
   services.cockpit = {
     enable = true;
     port = 9090;
@@ -22,10 +14,54 @@
     };
   };
 
-  # Vynucení poslouchání POUZE na IPv4 (0.0.0.0:9090)
+  # Vynucení poslouchání pouze na IPv4
   systemd.sockets.cockpit.socketConfig = {
     ListenStream = lib.mkForce [ "0.0.0.0:9090" ];
   };
 
   security.polkit.enable = true;
+
+  ############################################################
+  # 📊 Performance Co-Pilot (PCP) – historie metrik
+  ############################################################
+  services.pcp = {
+    enable = true;
+
+    # Zapnutí archivace (nutné pro historické grafy v Cockpitu)
+    archive = {
+      enable = true;
+
+      # Interval sběru metrik
+      # 10s = dobrý kompromis mezi přesností a velikostí dat
+      interval = "10s";
+    };
+  };
+
+  ############################################################
+  # 🧹 Retence a rotace logů PCP (aby ti to nesežralo disk)
+  ############################################################
+  systemd.services.pcp-archive-cleanup = {
+    description = "PCP archive cleanup";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.pcp}/bin/pmlogger_daily -K -E
+      '';
+    };
+  };
+
+  systemd.timers.pcp-archive-cleanup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+
+  ############################################################
+  # 📦 Užitečné nástroje pro debug / ruční kontrolu
+  ############################################################
+  environment.systemPackages = with pkgs; [
+    pcp   # pminfo, pmstat, pmrep...
+  ];
 }
