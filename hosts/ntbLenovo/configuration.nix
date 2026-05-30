633dd3c5b2aff2d98c956107754fea86349819a6
Gregor <gregorku@atlas.cz>
2026-05-30 18:20:44 +0200

    minio

--- hosts/ntbLenovo/configuration.nix
+++ hosts/ntbLenovo/configuration.nix
@@ -28,9 +28,9 @@
   networking.hostName = "ntbLenovo";
 
   #incus vypnutí minio, jinak nefunguje rebuild
-  nixpkgs.config.permittedInsecurePackages = [
-    "minio-2025-10-15T17-29-55Z"
-  ];
+  #nixpkgs.config.permittedInsecurePackages = [
+  #  "minio-2025-10-15T17-29-55Z"
+  #];
 
   # ----------------------
   # Lokalizace / Jazyk
