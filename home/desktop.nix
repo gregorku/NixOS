cc84b92a92c4aa7e29dcd496a3faf91e41f2ff1a
Gregor <gregorku@atlas.cz>
2026-06-07 19:45:07 +0200

    librewolf

--- home/desktop.nix
+++ home/desktop.nix
@@ -3,16 +3,13 @@
 {
   imports = [ ./common.nix ];
 
-programs.librewolf = {
-  enable = true;
-  languagePacks = [ "cs" ];
-
-  settings = {
-    "intl.locale.requested" = "cs";
-    "intl.multilingual.enabled" = false;
-    "privacy.resistFingerprinting.spoofLocale" = false;
-
-    # Firefox Standard místo Strict
-    "browser.contentblocking.category" = "standard";
+  programs.librewolf = {
+    enable = true;
+    languagePacks = [ "cs" ];
+    settings = {
+      "intl.locale.requested" = "cs";
+      "intl.multilingual.enabled" = false;
+      "privacy.resistFingerprinting.spoofLocale" = false;
+    };
   };
-};
+}
