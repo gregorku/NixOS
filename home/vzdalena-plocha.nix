16300294f272e08001da5228080477ddce0f97a9
Gregor <gregorku@atlas.cz>
2026-06-13 20:33:24 +0200

    plocha

--- home/vzdalena-plocha.nix
+++ home/vzdalena-plocha.nix
@@ -1,21 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  # Deklarativní nastavení pro nativní RDP v KDE Plasma 6
   xdg.configFile."krdprc".text = ''
     [RDP]
-    # Automatické spuštění serveru po přihlášení
     Enabled=true
-    # Port, na kterém krdp naslouchá (necháváme standardní)
     Port=3389
-    # Bezpečnost: Zakázání výzvy k potvrzení na monitoru v kanceláři
     RequireApprove=false
   '';
+  # Vynutíme přepsání krdprc, pokud by už existoval
+  xdg.configFile."krdprc".force = true;
 
-  # Volitelné: Ujistíme se, že KWin (Wayland) správně spolupracuje s PipeWire pro sdílení obrazu
   xdg.configFile."kwinrc".text = ''
     [Wayland]
-    # Aktivuje podporu pro virtuální výstupy a vzdálené sdílení
     VirtualOutputs=1
   '';
+  # Vynutíme přepsání kwinrc, což vyřeší naši chybu
+  xdg.configFile."kwinrc".force = true;
 }
