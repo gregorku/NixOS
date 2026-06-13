ab37cead02c8de54a340947d182dbbab84780261
Gregor <gregorku@atlas.cz>
2026-06-13 19:30:40 +0200

    plocha

--- modules/common-remote-access.nix
+++ modules/common-remote-access.nix
@@ -1,19 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  # 1. Aktivace vestavěné podpory RDP v KDE Plasma 6
-  services.krdp = {
-    enable = true;
-    openFirewall = false; # Pravidla firewallu řešíme níže
-  };
+  # 1. Přidání KRDP klienta/serveru do systémových balíčků
+  environment.systemPackages = with pkgs; [
+    kdePackages.krdp
+  ];
 
-  # 2. Nastavení firewallu (nftables) s restrikcí na rozsah vaší VPN
+  # 2. Nastavení firewallu (nftables) – povolení portů pouze z VPN rozsahu
   networking.firewall = {
     enable = true;
 
-    # Správný parametr pro nftables pravidla do řetězce input
     extraInputRules = ''
-      # Povolit RDP (TCP i UDP) pouze z VPN rozsahu
+      # Povolit RDP (TCP i UDP) pouze z VPN rozsahu pro KDE Plasma
       ip saddr 120.100.100.0/24 tcp dport 3389 accept
       ip saddr 120.100.100.0/24 udp dport 3389 accept
 
