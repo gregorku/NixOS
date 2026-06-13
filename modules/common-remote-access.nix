36896db5cbc19889b41336db0def04b98f40ee70
Gregor <gregorku@atlas.cz>
2026-06-13 19:29:38 +0200

    plocha

--- modules/common-remote-access.nix
+++ modules/common-remote-access.nix
@@ -1,28 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  # 1. Aktivace vestavěné podpory RDP v KDE Plasma 6 (Wayland)
+  # 1. Aktivace vestavěné podpory RDP v KDE Plasma 6
   services.krdp = {
     enable = true;
-    openFirewall = false; # Pravidla firewallu si definujeme bezpečně sami níže
+    openFirewall = false; # Pravidla firewallu řešíme níže
   };
 
-  # 2. Nastavení firewallu s restrikcí na rozsah vaší VPN (120.100.100.0/24)
+  # 2. Nastavení firewallu (nftables) s restrikcí na rozsah vaší VPN
   networking.firewall = {
     enable = true;
 
-    # Otevřeme porty pro RDP (3389) a SSH (22), ale POUZE z VPN rozsahu přes nftables
-    extraRulesAfterHooks = ''
-      table inet filter {
-        chain input {
-          # Povolit RDP (TCP i UDP) pouze z VPN rozsahu
-          ip saddr 120.100.100.0/24 tcp dport 3389 accept
-          ip saddr 120.100.100.0/24 udp dport 3389 accept
+    # Správný parametr pro nftables pravidla do řetězce input
+    extraInputRules = ''
+      # Povolit RDP (TCP i UDP) pouze z VPN rozsahu
+      ip saddr 120.100.100.0/24 tcp dport 3389 accept
+      ip saddr 120.100.100.0/24 udp dport 3389 accept
 
-          # Povolit SSH (TCP 22) pouze z VPN rozsahu
-          ip saddr 120.100.100.0/24 tcp dport 22 accept
-        }
-      }
+      # Povolit SSH (TCP 22) pouze z VPN rozsahu
+      ip saddr 120.100.100.0/24 tcp dport 22 accept
     '';
   };
 }
