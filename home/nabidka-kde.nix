4fcd6c07c5e4c5f0fa3ac4db5fde764520683c56
Gregor <gregorku@atlas.cz>
2026-06-17 19:46:50 +0200

    menu

--- home/nabidka-kde.nix
+++ home/nabidka-kde.nix
@@ -1,35 +1,8 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  xdg.desktopEntries.pracovniPc = {
-    name = "Pracovní PC";
-    exec = "/home/gregor/.ssh/pracePc-rdp.sh";
-    icon = "krdc";
-    terminal = false;
-    categories = [ "RemoteAccess" ];
-  };
-
-  xdg.desktopEntries.sluzebniPc = {
-    name = "Služební PC";
-    exec = "/home/gregor/.ssh/sluzebniPc-rdp.sh";
-    icon = "krdc";
-    terminal = false;
-    categories = [ "RemoteAccess" ];
-  };
-
-  xdg.desktopEntries.serverPcSync = {
-    name = "ServerPC-sync";
-    exec = "/home/gregor/.ssh/serverPc-sync.sh";
-    icon = "krdc";
-    terminal = false;
-    categories = [ "RemoteAccess" ];
-  };
-
-  xdg.desktopEntries.pracePc = {
-    name = "Práce PC";
-    exec = "/home/gregor/.ssh/pracePc.sh";
-    icon = "krdc";
-    terminal = false;
-    categories = [ "RemoteAccess" ];
-  };
+  imports = [
+    ./menu/vzdalena-plocha.nix
+    #./nabidka-kde.nix
+  ];
 }
