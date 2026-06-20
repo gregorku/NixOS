b7482fdcdc3063e5ce7266e8e909ce81e17e27b9
Gregor <gregorku@atlas.cz>
2026-06-20 20:22:26 +0200

    hdd

---
+++ home/menu/ntbLenovo/hdd.nix
@@ -0,0 +1,315 @@
+{ ... }:
+
+{
+
+# =========================
+
+# Kategorie HDD
+
+# =========================
+
+home.file.".local/share/desktop-directories/hdd.directory".text = ''
+[Desktop Entry]
+Type=Directory
+Name=HDD
+Icon=drive-harddisk-symbolic
+'';
+
+home.file.".local/share/desktop-directories/hdd-serveryPc.directory".text = ''
+[Desktop Entry]
+Type=Directory
+Name=Servery Pc HDD
+Icon=drive-harddisk-symbolic
+'';
+
+home.file.".local/share/desktop-directories/hdd-vps.directory".text = ''
+[Desktop Entry]
+Type=Directory
+Name=VPS HDD
+Icon=drive-harddisk-symbolic
+'';
+
+
+# =========================
+
+# Servery Pc
+
+# =========================
+
+home.file.".local/share/applications/hdd-server-doma.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Server Doma HDD
+Exec=/home/gregor/.ssh/hdd/hdd-server-doma.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-server-doma-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Server Doma Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-server-doma-root.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-serverPc-prace.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=ServerPC Práce HDD
+Exec=/home/gregor/.ssh/hdd/hdd-serverPc-prace.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-serverPc-prace-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=ServerPC Práce Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-serverPc-prace-root.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-virtPc-prace.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=VirtPC Práce HDD
+Exec=/home/gregor/.ssh/hdd/hdd-virtpc-prace.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-virtPc-prace-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=VirtPC Práce Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-virtpc-prace-root.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-testPc.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=TestPC HDD
+Exec=/home/gregor/.ssh/hdd/hdd-testpc.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-testPc-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=TestPC Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-testpc-root.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-pracovniPc-prace.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Pracovní PC HDD
+Exec=/home/gregor/.ssh/hdd/hdd-pracovniPc-prace.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-pracovniPc-prace-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Pracovní PC Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-pracovniPc-prace-root.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-raspberry-5b-prace.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Raspberry 5B Práce HDD
+Exec=/home/gregor/.ssh/hdd/hdd-raspberry-5b-prace.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-raspberry-5b-prace-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Raspberry 5B Práce Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-raspberry-5b-prace-root.sh
+Icon=drive-harddisk-symbolic
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-lenovo.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Lenovo HDD
+Exec=/home/gregor/.ssh/hdd/hdd-lenovo.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-nikola.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Nikola HDD
+Exec=/home/gregor/.ssh/hdd/hdd-nikola.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-nikola-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Nikola Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-nikola-root.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-raspberry-3b-nikola.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Raspberry 3B Nikola HDD
+Exec=/home/gregor/.ssh/hdd/hdd-raspberry-3b-nikola.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-raspberry-3b-nikola-root.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Raspberry 3B Nikola Root HDD
+Exec=/home/gregor/.ssh/hdd/hdd-raspberry-3b-nikola-root.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-virtpc-webmin.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=VirtPC Webmin HDD
+Exec=/home/gregor/.ssh/hdd/hdd-virtpc-webmin.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+
+# =========================
+
+# VPS HDD
+
+# =========================
+
+home.file.".local/share/applications/hdd-netcup-serverpc.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Netcup ServerPC
+Exec=/home/gregor/.ssh/hdd/hdd-netcup-serverpc.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-netcup-test-serverpc.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Netcup Test ServerPC
+Exec=/home/gregor/.ssh/hdd/hdd-netcup-test-serverpc.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-alwyzon-serverpc.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Alwyzon ServerPC
+Exec=/home/gregor/.ssh/hdd/hdd-alwyzon-serverpc.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-netcup-lxc-aplikace.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Netcup LXC Aplikace
+Exec=/home/gregor/.ssh/hdd/hdd-netcup-lxc-aplikace.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+# =========================
+
+# Routery Switche
+
+# =========================
+
+home.file.".local/share/applications/hdd-mikrotik-test.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Mikrotik Test
+Exec=/home/gregor/.ssh/hdd/hdd-mikrotik-test.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+
+home.file.".local/share/applications/hdd-cisco-jirkov.desktop".text = ''
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Cisco Jirkov
+Exec=/home/gregor/.ssh/hdd/hdd-cisco-jirkov.sh
+Icon=utilities-terminal
+Terminal=false
+StartupNotify=true
+'';
+}
