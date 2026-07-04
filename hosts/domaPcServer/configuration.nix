2937142bf58c6ef7873c34234c4dfe71dc3d4022
Gregor <gregorku@atlas.cz>
2026-07-04 15:02:38 +0200

    domapc

--- hosts/domaPcServer/configuration.nix
+++ hosts/domaPcServer/configuration.nix
@@ -3,37 +3,37 @@
 {
   imports = [
     ./hardware-configuration.nix
-    /etc/nixos/secrets/ssh
+    #/etc/nixos/secrets/ssh
 
     ##################################################
     # Common – sdílené moduly
     ##################################################
     ../../modules/common-security.nix
     ../../modules/common-server-swap.nix
-    ../../modules/common-snapshots.nix
+    #../../modules/common-snapshots.nix
 
     ##################################################
     # Server-only moduly
     ##################################################
     ../../modules/server/server-apps.nix
-    ../../modules/server/libvirt.nix
+    #../../modules/server/libvirt.nix
     ../../modules/server/cockpit.nix
     ../../modules/server/zfs.nix
-    ../../auto-upgrade.nix
+    #../../auto-upgrade.nix
 
     ##################################################
     # Server-networking
     ##################################################
-    ../../modules/server/br0-domaPcServer.nix
+    #../../modules/server/br0-domaPcServer.nix
 
     ##################################################
     # NSPAWN containers
     ##################################################
-    ../../containers/ha-doma/container.nix
-    ../../containers/caddy/container.nix
-    ../../containers/zigbee2mqtt/container.nix
-    ../../containers/jellyfin/container.nix
-    ../../containers/frigate/container.nix
+    #../../containers/ha-doma/container.nix
+    #../../containers/caddy/container.nix
+    #../../containers/zigbee2mqtt/container.nix
+    #../../containers/jellyfin/container.nix
+    #../../containers/frigate/container.nix
   ];
 
   ## =========================
@@ -57,25 +57,6 @@
   };
 
   ## =========================
-  ## HARDWARE / USB (Google Coral)
-  ## =========================
-  
-  # 1. Ovladače jádra (Gasket + Apex)
-  boot.extraModulePackages = [ config.boot.kernelPackages.gasket ];
-  boot.kernelModules = [ "gasket" "apex" ];
-
-  # 2. Udev pravidla z balíčku libedgetpu (TOTO NAHRÁVÁ FIRMWARE)
-  services.udev.packages = [ pkgs.libedgetpu ];
-
-  # 3. Vaše vlastní pravidla pro oprávnění (Ponecháno pro jistotu skupiny 'render')
-  services.udev.extraRules = ''
-  # Coral v bootloader režimu
-  SUBSYSTEM=="usb", ATTR{idVendor}=="1a6e", ATTR{idProduct}=="089a", MODE="0666", GROUP="users"
-  # Coral po nahrání firmwaru
-  SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="9302", MODE="0666", GROUP="users"
-  '';
-
-  ## =========================
   ## UŽIVATEL
   ## =========================
   users.users.gregor = {
@@ -84,9 +65,6 @@
     extraGroups = [
       "wheel"
       "networkmanager"
-      "libvirtd"
-      "render"   # PŘIDÁNO: Přístup k /dev/apex_0 a GPU
-      "plugdev"  # PŘIDÁNO: Obecný přístup k USB zařízením
     ];
   };
 
@@ -145,5 +123,5 @@
   ## =========================
   ## NIXOS KOMPATIBILITA
   ## =========================
-  system.stateVersion = "25.11";
+  system.stateVersion = "26.05";
 }
