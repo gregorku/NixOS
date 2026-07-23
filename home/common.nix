049d262c7da8d025257ca5dc5898570fa620a699
Gregor <gregorku@atlas.cz>
2026-07-23 20:26:12 +0200

    vs

--- home/common.nix
+++ home/common.nix
@@ -9,32 +9,4 @@
     nil
     nixfmt-rfc-style
   ];
-
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscodium;
-
-    mutableExtensionsDir = true;
-
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      jnoortheen.nix-ide
-      ms-python.python
-      yzhang.markdown-all-in-one
-    ];
-
-    userSettings = {
-      "editor.minimap.enabled" = false;
-      "editor.tabSize" = 2;
-      "files.autoSave" = "afterDelay";
-
-      "[nix]" = {
-        "editor.defaultFormatter" = "jnoortheen.nix-ide";
-      };
-
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings.nil.formatting.command" = [ "nixfmt" ];
-    };
-  };
 }
