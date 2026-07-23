c8a8fb8701de073dd5c6a851f5dba24a5eb03b62
Gregor <gregorku@atlas.cz>
2026-07-23 20:22:06 +0200

    vs

--- home/common.nix
+++ home/common.nix
@@ -4,4 +4,37 @@
   home.username = "gregor";
   home.homeDirectory = "/home/gregor";
   home.stateVersion = "26.05";
+
+  home.packages = with pkgs; [
+    nil
+    nixfmt-rfc-style
+  ];
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+
+    mutableExtensionsDir = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      jnoortheen.nix-ide
+      ms-python.python
+      yzhang.markdown-all-in-one
+    ];
+
+    userSettings = {
+      "editor.minimap.enabled" = false;
+      "editor.tabSize" = 2;
+      "files.autoSave" = "afterDelay";
+
+      "[nix]" = {
+        "editor.defaultFormatter" = "jnoortheen.nix-ide";
+      };
+
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "nix.serverSettings.nil.formatting.command" = [ "nixfmt" ];
+    };
+  };
 }
--- home/desktop.nix
+++ home/desktop.nix
@@ -29,4 +29,33 @@
       };
     };
   };
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+
+    mutableExtensionsDir = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      jnoortheen.nix-ide
+      ms-python.python
+      ms-vscode.cpptools
+      yzhang.markdown-all-in-one
+    ];
+
+    userSettings = {
+      "editor.fontLigatures" = true;
+      "editor.minimap.enabled" = false;
+      "editor.tabSize" = 2;
+      "files.autoSave" = "afterDelay";
+
+      "[nix]" = {
+        "editor.defaultFormatter" = "jnoortheen.nix-ide";
+      };
+
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "nix.serverSettings.nil.formatting.command" = [ "nixfmt" ];
+    };
+  };
 }
