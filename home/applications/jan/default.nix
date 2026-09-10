c71ddb5242c8f4cfbba38642440ff3054997bb1c
Your Name <you@example.com>
2026-09-10 13:31:07 +0200

    jan

--- home/applications/jan/default.nix
+++ home/applications/jan/default.nix
@@ -7,72 +7,24 @@
   janDataDir = "${config.home.homeDirectory}/.application-data/jan";
 
   janConfigDir = "${janDataDir}/config";
-
+  janDataHome = "${janDataDir}/data";
   janCacheDir = "${janDataDir}/cache";
 
   jan = pkgs.symlinkJoin {
     name = "jan-custom";
+    paths = [unstable.jan];
 
-    paths = [
-      unstable.jan
-    ];
-
-    nativeBuildInputs = [
-      pkgs.makeWrapper
-    ];
+    nativeBuildInputs = [pkgs.makeWrapper];
 
     postBuild = ''
-      wrapProgram "$out/bin/jan" \
+      wrapProgram "$out/bin/Jan" \
         --set XDG_CONFIG_HOME "${janConfigDir}" \
-        --set XDG_DATA_HOME "${janDataDir}" \
+        --set XDG_DATA_HOME "${janDataHome}" \
         --set XDG_CACHE_HOME "${janCacheDir}"
     '';
   };
 in {
-  # ------------------------------------------------------------
-  # Jan z nixpkgs unstable
-  # ------------------------------------------------------------
-
   home.packages = [
     jan
   ];
-
-  # ------------------------------------------------------------
-  # Jan data
-  #
-  # Všechna uživatelská data Jan jsou soustředěna pod:
-  #
-  # ~/.application-data/jan/
-  #
-  # ------------------------------------------------------------
-
-  home.activation.janDirectories = config.lib.dag.entryAfter ["writeBoundary"] ''
-    mkdir -p "${janConfigDir}"
-    mkdir -p "${janDataDir}"
-    mkdir -p "${janCacheDir}"
-  '';
-
-  # ------------------------------------------------------------
-  # Desktop launcher
-  #
-  # Používá náš wrapper, nikoliv přímo unstable.jan.
-  # Tím jsou stejné XDG cesty použity při spuštění z menu
-  # i z terminálu.
-  # ------------------------------------------------------------
-
-  xdg.desktopEntries.jan = {
-    name = "Jan";
-    genericName = "AI Assistant";
-    comment = "Open-source AI assistant";
-
-    exec = "${jan}/bin/jan %U";
-
-    icon = "jan";
-    terminal = false;
-
-    categories = [
-      "Utility"
-      "Development"
-    ];
-  };
 }
