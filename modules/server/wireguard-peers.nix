3bd291398ed63347eb6456d0e3608b203c9673a6
Your Name <you@example.com>
2026-09-19 22:41:18 +0200

    wireguard

---
+++ modules/server/wireguard-peers.nix
@@ -0,0 +1,161 @@
+{
+  doma = {
+    name = "Mikrotik doma";
+
+    wg1 = {
+      publicKey = "ZRRN9IVqc8atE1Cby4k00YKe0bd74N7/TkKZkIybKyk=";
+      allowedIP = "10.100.100.100/32";
+    };
+
+    wg2 = {
+      publicKey = "F4BOM0b5wukasvyv63EG6p61ZNMuwySSp09+j06iyHQ=";
+      allowedIP = "10.110.100.100/32";
+    };
+
+    wg3 = {
+      publicKey = "YHrYI2NdO8whuGxkEel1AixrvMVDz/KuMqmzI/v73QA=";
+      allowedIP = "10.120.100.100/32";
+    };
+  };
+
+  jirkov = {
+    name = "Mikrotik jirkov";
+
+    wg1 = {
+      publicKey = "vy/pvrKC55vhw1HDNrP/TnG3Y5mdaUcUsTOdJhRylWA=";
+      allowedIP = "10.100.100.220/32";
+    };
+
+    wg2 = {
+      publicKey = "OEnlmHsnNk+oIpvGOf8nF48r05G7PsCIjvMUJ8Cxd1Y=";
+      allowedIP = "10.110.100.220/32";
+    };
+
+    wg3 = {
+      publicKey = "HJmLfobWN7ZEjPt5PgeId0IUZBS+5odYCZATmR7whw4=";
+      allowedIP = "10.120.100.220/32";
+    };
+  };
+
+  bratrmach = {
+    name = "Mikrotik bratrmach";
+
+    wg1 = {
+      publicKey = "bozqiwT51C/Zhls/IBlKvHzYYQbAdUDJhB4pNoKdzEQ=";
+      allowedIP = "10.100.100.10/32";
+    };
+
+    wg2 = {
+      publicKey = "781kl7aR4tKfycU9hU30pzmD5sDpHgjpQPnWODPpWUk=";
+      allowedIP = "10.110.100.10/32";
+    };
+
+    wg3 = {
+      publicKey = "LK5kXiC0Ze7OiSjH+jgD+tcCfiAdPg6OqzMG34Lxm2A=";
+      allowedIP = "10.120.100.10/32";
+    };
+  };
+
+  klinovec = {
+    name = "Mikrotik klinovec";
+
+    wg1 = {
+      publicKey = "h0MYWHaQ0tZLv6MSJEb+QLX0MgJvJHs6wT0vDzqY2Qw=";
+      allowedIP = "10.100.100.210/32";
+    };
+
+    wg2 = {
+      publicKey = "BJrynnxoI/PkErV2IGLUw3YuolmJMOxKnP5Kx1y/8n4=";
+      allowedIP = "10.110.100.210/32";
+    };
+
+    wg3 = {
+      publicKey = "s9SmdTiHfIC17SApFbii+IHoZGi/tYk7tNVD0mrE6WA=";
+      allowedIP = "10.120.100.210/32";
+    };
+  };
+
+  prace = {
+    name = "Mikrotik prace";
+
+    wg1 = {
+      publicKey = "67t2MwYpEsjDZUYs2cwmeWImcJE+v/r+z8MT47jJEDU=";
+      allowedIP = "10.100.100.12/32";
+    };
+
+    wg2 = {
+      publicKey = "RQ0tpTCNb6XJLBAb/KeTe66+xGAWMGoro6n3JIDvq0w=";
+      allowedIP = "10.110.100.12/32";
+    };
+
+    wg3 = {
+      publicKey = "G6Pf0bPmFXY9cQOoT1MERNY58ZisX9sL9wkFLd8DGh8=";
+      allowedIP = "10.120.100.12/32";
+    };
+  };
+
+  test = {
+    name = "Mikrotik test";
+
+    wg1 = {
+      publicKey = "4gCXT1X3S25rmRBAqSbC9fQLFCARJHfSqwUhlOyIKgw=";
+      allowedIP = "10.100.100.5/32";
+    };
+
+    wg2 = {
+      publicKey = "IAPBFkq+wBhbUzfXiSa+yMyXvntiVXOt1r/zLB996Sk=";
+      allowedIP = "10.110.100.5/32";
+    };
+
+    wg3 = {
+      publicKey = "UDL0tHuDl0gEoUa6G/zM31fgjiIG6CdGnsVPqmo18V8=";
+      allowedIP = "10.120.100.5/32";
+    };
+  };
+
+  udlice = {
+    name = "Mikrotik udlice";
+
+    wg1 = {
+      publicKey = "f9JErcFF6j5DRZnSmFtr89lWOiSBSvrXIdFB29Nw3Fs=";
+      allowedIP = "10.100.100.200/32";
+    };
+
+    wg2 = {
+      publicKey = "uPV1wEMUbTGtPD2nYFpemg8paucEOGIOhMnHGJr4WSw=";
+      allowedIP = "10.110.100.200/32";
+    };
+
+    wg3 = {
+      publicKey = "2JhsoHf/VXltiU6NJFhU3Rc0bt2hHJy2wJOcB8RZ11c=";
+      allowedIP = "10.120.100.200/32";
+    };
+  };
+
+  mujmobil = {
+    name = "Mobil mujmobil";
+
+    wg1 = {
+      publicKey = "J6EufZFy3mb7wyCV6vP/XvrEEZLd4THYYzcKiB+THzo=";
+      allowedIP = "10.100.100.152/32";
+    };
+  };
+
+  ntblenovo = {
+    name = "Notebook ntblenovo";
+
+    wg1 = {
+      publicKey = "hDKHmFQ0SyFgOzAoZQb4ywb0PwMyJAlqwXyuC3+oATU=";
+      allowedIP = "10.100.100.150/32";
+    };
+  };
+
+  ntbpracovni = {
+    name = "Notebook ntbpracovni";
+
+    wg1 = {
+      publicKey = "aoNFdXvTciXud3FlzJZB0uy1ZPlmK3CgKiwFcyb4lX8=";
+      allowedIP = "10.100.100.151/32";
+    };
+  };
+}
